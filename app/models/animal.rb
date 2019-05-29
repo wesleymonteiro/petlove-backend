417264d9b92716b5d2c5e38bdc07b5a213dd676b
Unknown <weslito96@gmail.com>
2019-05-29 19:27:10 -0300

    create validations

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,3 +1,28 @@
 class Animal < ApplicationRecord
   belongs_to :curator
+
+  validates :name, :kind, :curator, :monthly_cost, presence: true
+  validate :is_curator_18_years_old?, if: -> { kind == "Andorinha" }
+  validate :curator_name_starts_with_a?, if: -> { kind == "Gato" }
+  validate :curator_monthly_costs_higher_than_1000?
+
+  before_save :camelcase_names
+
+  private
+  def camelcase_names
+    name = name.camelcase
+    kind = kind.camelcase
+  end
+
+  def is_curator_18_years_old?
+    errors.add(:curator, :invalid_age_for_kind) if Date.current.year - curator.birthday.year > 18
+  end
+
+  def curator_name_starts_with_a?
+    errors.add(:curator, :invalid_name_for_kind) if curator.name[0] == "A"
+  end
+
+  def curator_monthly_costs_higher_than_1000?
+    errors.add(:curator, :high_monthly_cost) if curator.animals.pluck(:monthly_cost).reduce(:+) > 1000
+  end
 end
--- app/models/curator.rb
+++ app/models/curator.rb
@@ -1,3 +1,11 @@
 class Curator < ApplicationRecord
   has_many :animals
+
+  validates :name, :document, :birthday, presence: true
+  before_save :camelcase_name
+
+  private
+  def camelcase_name
+    name = name.camelcase
+  end
 end
