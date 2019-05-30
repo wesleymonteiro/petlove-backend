0d79cb2e4e699720506ae84647321cc51e6fbc6c
Unknown <weslito96@gmail.com>
2019-05-29 21:26:05 -0300

    create index and show answers for defined questions

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,11 @@
+class QuestionsController < ApplicationController
+  def index
+    dogs = Animal.where(kind: "Cachorro") 
+    @dogs_monthly_cost_average = dogs.pluck(:monthly_cost).instance_eval { reduce(:+) / size.to_f }
+    @dogs_size = dogs.size
+    @dogs_names = dogs.pluck(:name)
+    curators = Curator.joins(:animals).select('curators.*, sum(animals.monthly_cost) as total_cost, sum(animals.monthly_cost) * 3 as future_cost').group("curators.id").reorder("total_cost desc")
+    @reordened_curators = curators
+    @three_months_costs = curators.map { |curator| {"#{curator.name}": curator.future_cost } }
+  end
+end
